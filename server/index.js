52345c3046685c61d0b2fc696e30ac5fdb5b3423
satxnishere <satxnhere@gmail.com>
2024-12-13 19:40:06 +0530

    Complete Server Setup

--- server/index.js
+++ server/index.js
@@ -14,7 +14,25 @@ const app = express();
 const port =  process.env.Port || 3000;
 const databaseURL = process.env.database_URL;
 
+//Middleware to Communicate between different
+app.use(cors({
+    origin : [process.env.ORIGIN],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials : true,
+}));
 
+// Used for getting the cookies from the frontend
+app.use(cookieParser());
+
+// Body Parser
+app.use(express.json());
+
+// Routes
 const server = app.listen(port, ()=>{
     console.log(`Server is running at http://localhost:${port}`);
+})
+
+// Database Connection
+mongoose.connect(databaseURL).then(()=>{
+    console.log('DB Connection Successful!');
 })
