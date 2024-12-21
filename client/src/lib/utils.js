14bfa3b06a37c2dbd807e697ddb8e7e87bb824f2
satxnishere <satxnhere@gmail.com>
2024-12-22 00:27:43 +0530

    Chat Feature UI DESIGN 1/2

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -1,6 +1,6 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge"
-
+import animationData from '@/assets/lottie-json'
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
@@ -18,4 +18,10 @@ export const getColor = (color) => {
   }
 
   return colors[0]; // Fallback to the first color if out of range
+};
+
+export const animationDefaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData,
 };
