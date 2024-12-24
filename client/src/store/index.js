cdf97ede66f3e942750912f4ad1783580a2b5648
jadhavharshh <harshjadhavconnect@gmail.com>
2024-12-25 00:18:17 +0530

    Add chat slice to state management: include selected chat messages and close chat functionality

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,9 @@
 import { create } from "zustand";
 import { createAuthSlice } from "./slices/auth-slice";
+import { chatSlice } from "./slices/chat-slice";
 
 
 export const userAppStore = create()((...a) =>({
     ...createAuthSlice(...a),
+    ...chatSlice(...a),
 }))
